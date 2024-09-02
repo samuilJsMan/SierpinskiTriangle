ab77c0a291ebd30788d280fe25fe92291b69f7c8
samuilJsMan <132012875+samuilJsMan@users.noreply.github.com>
2024-09-02 19:07:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,21 @@
-"use strict";
+"use strict"
 
-let whereToAddPoint=document.querySelector(`.mainBoard`),Vertical0Andhorizontal1,itterations=0,randomize,
-newPointAxels,axelFrom,axelTo,axelWhereToStand,previousPointAxel,newPoint,startPoint,
-destinationPoints=[[404.133,0],[-202.666,350],[-202.666,-350]];
+const canvas=document.querySelector(`canvas`) 
+const sideSize=(window.innerHeight>window.innerWidth?window.innerWidth:window.innerHeight)*0.95
+canvas.width=sideSize+3                                                                        
+const triHeight=Math.sqrt(sideSize**2-((sideSize)/2)**2)                                        
+canvas.height=triHeight+3                                                                      
+const triCorners=[[sideSize/2,0],[sideSize,triHeight],[0,triHeight]]                                                                                
+const canvasReady=canvas.getContext(`2d`)                             
+let lastPoint=[sideSize/2,0]
+setTimeout(()=>{setInterval(newPoint,1)},1000)
 
-startPoint=document.createElement(`div`)
-startPoint.classList.add(`axel0`,`axel`)
-startPoint.style.bottom=`${destinationPoints[2][0]}px`;
-startPoint.style.left=`${destinationPoints[2][1]}px`;
-whereToAddPoint.appendChild(startPoint)
-
-setTimeout(serpinsky,1000)
-function serpinsky(){
-	if(itterations<3000){Vertical0Andhorizontal1=0
-		randomize=Math.floor(Math.random()*3)
-		newPoint=document.createElement(`div`)
-		newPoint.classList.add(`axel${itterations+1}`,`axel`)
-		whereToAddPoint.appendChild(newPoint)
-		newPointAxels=document.querySelector(`.axel${itterations+1}`)
-		while(Vertical0Andhorizontal1!=2){
-		if(Vertical0Andhorizontal1==0){axelFrom=window.getComputedStyle
-			(document.querySelector(`.axel${itterations}`)).getPropertyValue(`bottom`)
-		}else{axelFrom=window.getComputedStyle
-			(document.querySelector(`.axel${itterations}`)).getPropertyValue(`left`)}
-		axelFrom=parseInt(axelFrom)
-		axelTo=destinationPoints[randomize][Vertical0Andhorizontal1]
-		if (axelFrom>0 && axelTo>0){
-			axelWhereToStand=axelFrom-(axelFrom-axelTo)/2
-		}else if(axelFrom<0&&axelTo<0){
-			axelWhereToStand=axelFrom+(Math.abs(axelFrom)-Math.abs(axelTo))/2
-		}else{
-			if(axelFrom>axelTo){axelWhereToStand=axelFrom-(axelFrom+Math.abs(axelTo))/2}
-			if(axelFrom<axelTo){axelWhereToStand=axelFrom+(Math.abs(axelFrom)+axelTo)/2}
-		}
-		axelWhereToStand=axelWhereToStand+`px`
-		if(Vertical0Andhorizontal1==0){
-			newPointAxels.style.bottom=axelWhereToStand
-		}else{
-			newPointAxels.style.left=axelWhereToStand
-		}
-		Vertical0Andhorizontal1++}
-		itterations++
-		setTimeout(serpinsky,1)
-	}		
+function newPoint(){                                                                        
+    let RD=Math.floor(Math.random()*3)                                                        
+    lastPoint=[lastPoint[0]+(triCorners[RD][0]-lastPoint[0])/2,lastPoint[1]+(triCorners[RD][1]-lastPoint[1])/2]
+    let r=(36+(10/triHeight*lastPoint[1]))*0.66 + (36+(10/sideSize*lastPoint[0]))*0.33
+    let g=(77+(100/triHeight*lastPoint[1]))*0.66 + (77+(100/sideSize*lastPoint[0]))*0.33
+    let b=(212+(-110/triHeight*lastPoint[1]))*0.66 + (212+(-110/sideSize*lastPoint[0]))*0.33
+    canvasReady.fillStyle=`rgb(${r},${g},${b})`                     
+    canvasReady.fillRect(lastPoint[0],lastPoint[1],1,1)
 }
